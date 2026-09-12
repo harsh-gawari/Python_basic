a59b19ebc10821edfa588d300333be805766b529
Harsh Gawari <harshgawari42@gmail.com>
2026-09-12 13:32:19 +0530

    understand the comments in pyhton

---
+++ comments.py
@@ -0,0 +1,9 @@
+# there are two types of comments 
+# 1] single line comments :
+#   (#) token  is used to  single line comments 
+
+A,B ="harsh",4 
+#print((A)*B)  
+
+# 2]multiline comments :
+"""  paragrph multiline comments  """
