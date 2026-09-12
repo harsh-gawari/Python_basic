45de7b8f8e8084780f5296f8d5cc98c86b8ba28e
Harsh Gawari <harshgawari42@gmail.com>
2026-09-12 23:06:50 +0530

    understanding input output

---
+++ inputoutput.py
@@ -0,0 +1,29 @@
+# input are used to  accept value from  user
+
+name = input("name :")
+
+
+# perticular data type input
+
+age = int(input("Enter the age:"))
+print(name)
+print("My age is:",age)
+
+salary = float(input("Enter the salary:"))
+print(salary)
+
+# Exercise 1. Accept numbers from a user and add
+
+num1 = int(input("Enter num 1 :"))
+num2 = int(input("Enter num 2 :"))
+print("Addition :",num1+num2)
+
+
+
+# operator presidence 
+# not > and > or 
+# cosider the expresion (not true and false or true)
+# higher presedence not true --> false
+# then and  false and false ---> true
+# then  true and true ---> true 
+# final output ---> true 
