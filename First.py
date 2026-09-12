2abf9dfba39c32ba14419b7e032ee2cd5ccbae83
Harsh Gawari <harshgawari42@gmail.com>
2026-09-12 11:46:34 +0530

    first commit

---
+++ First.py
@@ -0,0 +1,30 @@
+
+# printing syntax of python
+
+print("hello world")
+
+print(3+10)
+# print("51"+13) -->  type error
+
+
+# variable declaration 
+ 
+name = "harsh"
+age = 25 
+salary = 26000.00
+
+# checking the variable type 
+
+print(type(name))
+print(type(age))
+print(type(salary))
+
+
+# Data types
+ 
+# Integer 
+# String 
+# Float 
+# Boolean
+# None
+
